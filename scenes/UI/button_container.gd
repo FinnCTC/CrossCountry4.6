d2ad7ded6d1924467255f9f175e52c17cd548ea8
Ca-mP <ckpatterson1s@capetigers.com>
2026-04-02 13:17:31 -0500

    Added system to navigate UI with controller

--- scenes/UI/button_container.gd
+++ scenes/UI/button_container.gd
@@ -1,62 +1,74 @@
 extends VBoxContainer
 class_name ButtonContainer
 
-var button_rows: Array[ButtonContainerRow] = []
-
 enum BUTTON_MODES {MOUSE, CONTROLLER}
 
 var button_mode: BUTTON_MODES = BUTTON_MODES.MOUSE
 
-var buttons_array = []
+var buttons_array: Array
 
-var x_index: int = 0
-var y_index: int = 0
-
-var x_amounts: Array[int] = [] # an array containing the amounts of buttons in each row
-var y_amount: int = 0
+var selected_button_index: Vector2
 
 func _ready() -> void:
-	var rows_count = 0
-	for child in self.get_children():
-		if child is ButtonContainerRow:
-			button_rows.append(child)
-			
-			var buttons_in_row: int = child.get_children().size()
-			x_amounts.append(buttons_in_row)
-			
-			for button in child.get_children():
-				if button is LevelButton:
-					buttons_array.append(button)
-			
-			y_amount += 1
+	buttons_array = create_buttons_array()
 
 func _process(delta: float) -> void:
 	if button_mode == BUTTON_MODES.CONTROLLER:
-		if Input.is_action_just_pressed("move_right"):
-			if x_amounts[y_index] > x_index:
-				x_index += 1
-				focus_button(x_index, y_index)
-		if Input.is_action_just_pressed("move_left"):
-			if x_index > 1:
-				x_index -= 1
-				focus_button(x_index, y_index)
-		if Input.is_action_just_pressed("move_forward"):
-			if y_index > 1:
-				y_index -= 1
-				focus_button(x_index, y_index)
 		if Input.is_action_just_pressed("move_back"):
-			if y_index < y_amount:
-				y_index += 1
-				focus_button(x_index, y_index)
+			move_selection_vertical(1)
+
+func move_selection_vertical(direction: int):
+	var start = selected_button_index.y + direction
+	start = start.clamp(0, buttons_array.size() - 1)
+	
+	var end = buttons_array.size() if direction > 0 else -1
+	
+	for i in range(start, end, direction):
+		if buttons_array[i].size() > selected_button_index.x:
+			selected_button_index.y = i
+			break
+	
+	var next_row = buttons_array[start]
+	var last_button = next_row[-1]
+	selected_button_index = Vector2(next_row.size() - 1, start)
+
+func move_selection_horizontal(direction: int):
+	var current_row = buttons_array[selected_button_index.y]
+	
+	if direction < 0 and selected_button_index.x == 0:
+		selected_button_index = current_row.size() - 1
+		return
+	
+	if direction > 0 and selected_button_index.x >= current_row.size() - 1:
+		selected_button_index.x = 0
+		return
+	
+	selected_button_index.x += direction
 
 func _unhandled_input(event: InputEvent) -> void:
 	if event is InputEventJoypadButton or event is InputEventJoypadMotion:
 		button_mode = BUTTON_MODES.CONTROLLER
-		x_index = 1
-		y_index = 1
+		selected_button_index = Vector2(1, 1)
+
+func create_buttons_array() -> Array:
+	var buttons_array: Array[Array] = []
+	var rows_amount := 0
+	for child in self.get_children():
+		if child is ButtonContainerRow:
+			var button_row = child
+			var array_row: Array[Dictionary] = []
+			
+			for grandchild in button_row.get_children():
+				if grandchild is LevelButton:
+					var button = grandchild
+					var button_object = {"button": button, "is_selected": false}
+					array_row.append(button_object)
+			
+			buttons_array.append(array_row)
+	return buttons_array
 
 func focus_button(button_x: int, button_y):
-	%AnimationPlayer.get_animation("new_animation")
+	pass
 
 func unfocus_button():
 	pass
